711fb9c0da8b62138d647506e7ff802abcc9cdfd
Chilaka Ramakrishna <ramakrishna@nference.net>
2020-12-19 21:08:16 +0530

    reset functionality

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,27 @@ impl CPU {
         self.memory[addr as usize]
     }
 
+    fn mem_read_u16(&self, addr: u16) -> u16 {
+        let low = self.memory[addr as usize] as u16;
+        let high = self.memory[addr + 1 as usize] as u16;
+        high << 8 | (low as u16)
+    }
+
+    fn mem_write_u16(&mut self, addr: u16, data: u16) {
+        let high = (data >> 8) as u8;
+        let low = data as u8;
+        self.mem_write(pos, low);
+        self.mem_write(pos + 1, high);
+    }
+
+    fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
     fn mem_write(&mut self, addr: u16, data: u8) {
         self.memory[addr as usize] = data;
     }
@@ -56,6 +77,7 @@ impl CPU {
 
     pub fn load_and_run(&mut self, program: Vec<u8>) {
         self.load(program);
+        self.reset();
         self.run();
     }
 
@@ -88,58 +110,4 @@ impl CPU {
 fn main() {
     let x = 2;
     println!("Hello, world! {}", x);
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_0xa9_lda_immediate_load_data() {
-        let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x05, 0x00]);
-        assert_eq!(cpu.register_a, 5);
-        assert_eq!(cpu.status & 0b0000_0010, 0);
-        assert_eq!(cpu.status & 0b1000_0000, 0);
-    }
-
-    #[test]
-    fn test_0xa9_lda_zero_flag() {
-        let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
-        assert_eq!(cpu.status & 0b0000_0010, 0b10);
-    }
-
-    #[test]
-    fn test_0xa9_lda_negative_flag() {
-        let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0xff, 0x00]);
-        assert_eq!(cpu.status & 0b1000_0000, 0b1000_0000);
-    }
-
-    #[test]
-    fn test_0xaa_tax_move_a_to_x() {
-        let mut cpu = CPU::new();
-        cpu.register_a = 10;
-        cpu.interpret(vec![0xaa, 0x00]);
-
-        assert_eq!(cpu.register_x, 10)
-    }
-
-    #[test]
-    fn test_5_ops_working_together() {
-        let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
-
-        assert_eq!(cpu.register_x, 0xc1)
-    }
-
-    #[test]
-    fn test_inx_overflow() {
-        let mut cpu = CPU::new();
-        cpu.register_x = 0xff;
-        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
-
-        assert_eq!(cpu.register_x, 1)
-    }
 }
